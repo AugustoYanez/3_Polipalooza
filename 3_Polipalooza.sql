4f57c7ef77a09c9016906ca9a4da84d834d8ff28
MaximoColombatto4ac <maximo.colombatto@gmail.com>
2023-06-21 09:47:04 -0300

    base de datos e inserts

---
+++ 3_Polipalooza.sql
@@ -0,0 +1,4 @@
+call escenarioArtistas;
+call rolPersonas;
+select Polipalooza.personalMinimo(1);
+select generoMayor();
---
+++ script 2.sql
@@ -0,0 +1,32 @@
+insert into Escenarios values(0, null, null);
+insert into Escenarios values(1, null, null);
+
+insert into Personas values(0, NULL, NULL, NULL, NULL);
+insert into Personas values(1, NULL, NULL, NULL, NULL);
+insert into Personas values(2, NULL, NULL, NULL, NULL);
+insert into Personas values(3, NULL, NULL, NULL, NULL);
+insert into Personas values(4, NULL, NULL, NULL, NULL);
+insert into Personas values(5, NULL, NULL, NULL, NULL);
+insert into Personas values(6, NULL, NULL, NULL, NULL);
+insert into Personas values(7, NULL, NULL, NULL, NULL);
+
+insert into Artistas values(0, 0, NULL, NULL);
+insert into Artistas values(1, 1, NULL, NULL);
+insert into Artistas values(5, 5, "rock", NULL);
+insert into Artistas values(6, 6, "rock", NULL);
+insert into Artistas values(7, 7, "rock", NULL);
+
+insert into Presentaciones values(0, 0, 0, NULL, NULL);
+insert into Presentaciones values(1, 0, 1, NULL, NULL);
+
+insert into roles values(null,'iluminacion');
+insert into roles values(null,'sonido');
+insert into roles values(null,'tecnico');
+
+insert into PersonalProduccion values(0, 2, 0);
+insert into PersonalProduccion values(1, 3, 1);
+insert into PersonalProduccion values(2, 4, 2);
+
+insert into ProduccionEscenarios values(0, 0);
+insert into ProduccionEscenarios values(0, 1);
+insert into ProduccionEscenarios values(0, 2);
