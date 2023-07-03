d8cd27ce55123c80754baeb2d8cab47231f801bf
MaximoColombatto4ac <maximo.colombatto@gmail.com>
2023-07-03 08:36:08 -0300

    pasando el branch entrega final a mi branch

--- polipalozaEstructura3.sql
+++ polipalozaEstructura3.sql
@@ -15,11 +15,12 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+create database Polipalooza;
+use Polipalooza;
 --
 -- Table structure for table `Artistas`
 --
-create database Polipalooza;
-use Polipalooza;
 
 DROP TABLE IF EXISTS `Artistas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -342,6 +343,30 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `escenarioPersonal` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`alumno`@`localhost` PROCEDURE `escenarioPersonal`(in id int)
+BEGIN
+select PersonalProduccion.personal_id, nombreRol
+from Escenarios
+join ProduccionEscenarios on ProduccionEscenarios.escenario_id = Escenarios.escenario_id
+join PersonalProduccion on PersonalProduccion.personal_id = ProduccionEscenarios.personal_id
+join roles on rol = roles_rol
+where Escenarios.escenario_id = id;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `personalHorarios` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -416,4 +441,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-30 15:12:37
+-- Dump completed on 2023-06-30 16:54:35
